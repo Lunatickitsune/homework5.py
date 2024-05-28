21683860415ba78b4ac7d14bdb61e7654f547e01
valeriia <148711262+Lunatickitsune@users.noreply.github.com>
2024-05-29 01:11:21 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+immutable_var = 1, "Валерия", 2.15, True
+print(immutable_var)
+#immutable_var[0] = 3  кортеж нельзя изменить
+print(immutable_var)
+mutable_list = [1,3,'s','d']
+print(mutable_list)
+mutable_list[2] = 23
+print(mutable_list)
